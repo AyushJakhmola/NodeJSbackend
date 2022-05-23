1ad6a97c4adb5a9ac898e782e54021a8a1be039b
Ayush Jakhmola <ayushjakhmola001@gmail.com>
2022-05-23 17:17:30 +0530

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get update
+#apt-get install net-tools
+#curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash 
+#apt-get install gcc make -y
+#apt-get install nodejs -y
+#npm  install pm2@latest -g
+
+cd /home/ubuntu/NodeJSbackend
+sudo apt install npm -y
+sudo npm install npm -g
+sudo npm install -d
+sudo npm install pm2@latest -g
