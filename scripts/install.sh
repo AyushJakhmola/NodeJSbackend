f72f556cae6388e53276670d7ab2b6e87f9e7635
Ayush Jakhmola <ayushjakhmola001@gmail.com>
2022-06-13 20:13:38 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install net-tools
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install gcc make -y
-sudo apt-get install nodejs -y
-sudo apt-get upgrade -y
-sudo npm install pm2@latest -g
-npm install
+apt-get update
+#apt-get install net-tools
+#curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash 
+#apt-get install gcc make -y
+#apt-get install nodejs -y
+#npm  install pm2@latest -g
 
+cd /home/ubuntu/NodeJSbackend
+sudo apt install npm -y
+sudo npm install npm -g
+sudo npm install -d
+sudo npm install pm2@latest -g
